90eb8fcf8fa34f0cc874c31b7f23410f373d7c91
Ahmad Awali <aawali73@yahoo.com>
2022-11-01 02:20:49 +0200

    Add project files.

---
+++ SunRiseTemplate/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SunRiseTemplate.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
